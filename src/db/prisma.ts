a66086fabfc94984b72de447be87a1a10b8c3a54
nasifayc <nasifayc11@gmail.com>
2025-03-29 11:22:03 +0300

    feat: prisma setup

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,5 +1,7 @@
-// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+import { PrismaClient } from "@prisma/client";
 
-// export const prisma = globalForPrisma.prisma || new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
