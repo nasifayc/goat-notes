03f49ee510ac2d41a73f07cf7581b34a6e63d948
nasifayc <nasifayc11@gmail.com>
2025-04-02 14:55:16 +0300

    feat: AI note analaysis added

--- src/components/AskAIButton.tsx
+++ src/components/AskAIButton.tsx
@@ -1,13 +1,137 @@
 "use client";
 
 import { User } from "@supabase/supabase-js";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import React, { Fragment, useRef, useState, useTransition } from "react";
+import { Router } from "next/router";
+import { useRouter } from "next/navigation";
+import { Textarea } from "./ui/textarea";
+import { ArrowUpIcon } from "lucide-react";
 
 type Props = {
   user: User | null;
 };
 function AskAIButton({ user }: Props) {
-  console.log(user?.email);
-  return <div>AskAIButton</div>;
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const [questionText, setQuestionText] = useState("");
+  const [questions, setQuestions] = useState<string[]>([]);
+  const [responses, setResponses] = useState<string[]>([]);
+
+  const [isPending, startTransition] = useTransition();
+
+  const handleOnOpenChange = (isOpen: boolean) => {
+    if (!user) {
+      router.push("/login");
+    } else {
+      if (isOpen) {
+        setQuestionText("");
+        setQuestions([]);
+        setResponses([]);
+      }
+      setOpen(isOpen);
+    }
+  };
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const handleInput = () => {
+    const textArea = textareaRef.current;
+
+    if (!textArea) return;
+
+    textArea.style.height = "auto";
+    textArea.style.height = `${textArea.scrollHeight}px`;
+  };
+
+  const handleClickInput = () => {
+    textareaRef.current?.focus();
+  };
+
+  const handleSubmit = async () => {};
+
+  const scrollToBottom = () => {
+    contentRef.current?.scrollTo({
+      behavior: "smooth",
+      top: contentRef.current?.scrollHeight,
+    });
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit();
+    }
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleOnOpenChange}>
+      <DialogTrigger asChild>
+        <Button variant="secondary">Ask AI</Button>
+      </DialogTrigger>
+      <DialogContent
+        className="custom-scrollbar flex h-[85vh] max-w-4xl flex-col overflow-y-auto"
+        ref={contentRef}
+      >
+        <DialogHeader>
+          <DialogTitle>Ask AI About Your Notes</DialogTitle>
+          <DialogDescription>
+            Our AI can answer questions about all of your notes
+          </DialogDescription>
+        </DialogHeader>
+        <div className="mt-4 flex flex-col gap-8">
+          {questions.map((question, index) => (
+            <Fragment key={index}>
+              <p className="bg-muted text-muted-foreground ml-auto max-w-[60%] rounded-md px-2 py-1 text-sm">
+                {question}
+              </p>
+              {responses[index] && (
+                <p
+                  className="bot-response text-sm"
+                  dangerouslySetInnerHTML={{ __html: responses[index] }}
+                />
+              )}
+            </Fragment>
+          ))}
+
+          {isPending && <p className="tetx-sm animate-pulse">Thinking...</p>}
+        </div>
+        <div
+          className="curso-text mt-auto flex flex-col rounded-lg border p-4"
+          onClick={handleClickInput}
+        >
+          <Textarea
+            ref={textareaRef}
+            placeholder="Ask me anything about your notes..."
+            className="placeholder:text-muted-foreground resize-none rounded-none border-none bg-transparent p-0 shadow-none focus-visible:ring-0 focus-visible:ring-offset-0"
+            style={{
+              minHeight: "0",
+              lineHeight: "normal",
+            }}
+            rows={1}
+            onInput={handleInput}
+            onKeyDown={handleKeyDown}
+            value={questionText}
+            onChange={(e) => setQuestionText(e.target.value)}
+          />
+        </div>
+        <Button className="ml-auto size-8 rounded-full">
+          <ArrowUpIcon className="text-background" />
+        </Button>
+      </DialogContent>
+    </Dialog>
+  );
 }
 
 export default AskAIButton;
